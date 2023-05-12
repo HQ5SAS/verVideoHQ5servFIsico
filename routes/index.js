0b51d74331c370cdd3fb0ceea9fd83521991aa10
NataliaCor <88517729+NataliaCor@users.noreply.github.com>
2023-05-12 17:30:58 -0500

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/videoServer', function (req, res, next) {
+  const ID_user = req.query.id;
+  const range = req.headers.range;
+  if(!range){
+    res.status(400).send("err requiere range header")
+  }
+  const videoPath='/transfdhq5/'+ID_user;
+  const videoSize= fs.statSync(videoPath).size;
+
+  const CHUNNK_SIZE= 60 ** 6;//6M 
+  const start= Number(range.replace(/\D/g, ""));
+  const end = Math.min(start+CHUNNK_SIZE, videoSize -1);
+
+  const contentLength = end - start +1;
+  const headers={
+    "Content-Range":`bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges":"bytes",
+    "Content-Length": contentLength,
+    "Content-Type":"video/mp4"
+  };
+  res.writeHead(206, headers);
+  const videoStream= fs.createReadStream(videoPath,{start, end});
+  videoStream.pipe(res)
+
+})
+//-else------------------
+router.get('/*', function (req, res, next) {
+  res.render('404err', { title: 'error 404' })
+})
